df5ef4536e4c8ec1aaae88a0622c35bab6457643
gcapcha <gcapcha@tumi-soft.com>
2020-11-19 10:30:08 -0500

    Feature: A

---
+++ Dashboard/Filters/FilterBySales.php
@@ -0,0 +1,11 @@
+<?php
+namespace Modules\Dashboard\Filters;
+
+class FilterBySales{
+    public function saleFilter($list = [])
+    {
+        foreach ($list as $key => $value) {
+            # code...
+        }
+    }
+}
--- Dashboard/Producer/Producer.php
+++ Dashboard/Producer/Producer.php
@@ -1,1 +1,15 @@
 <?php
+namespace Modules\Dashboard\Producer;
+use App\Models\SaleDocument;
+use Modules\Dashboard\Filters\FilterBySales;
+
+class Producer{
+    public function getSales(){
+        $list = SaleDocument::select(SaleDocument::TABLE_NAME . '.*')
+            ->whereNull(SaleDocument::TABLE_NAME . '.deleted_at')
+            ->where(SaleDocument::TABLE_NAME . '.flag_active', SaleDocument::STATE_ACTIVE)
+            ->get();
+        $filterBySales = new FilterBySales();
+        $filterBySales->saleFilter($list);
+    }
+}
