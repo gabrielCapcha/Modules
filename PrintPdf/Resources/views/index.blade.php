37624f82ab46c054f22b3dc8bcdc4fa132fb3133
gcapcha <gcapcha@tumi-soft.com>
2020-11-19 09:45:55 -0500

    refactor

--- PrintPdf/Resources/views/index.blade.php
+++
@@ -1,9 +0,0 @@
-@extends('printpdf::layouts.master')
-
-@section('content')
-    <h1>Hello World</h1>
-
-    <p>
-        This view is loaded from module: {!! config('printpdf.name') !!}
-    </p>
-@endsection
